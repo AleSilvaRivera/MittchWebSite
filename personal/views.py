9eccb4973cd893ecbd2e47d7cdad256dacc12ac2
Paulis <alesilvacorreobasura@gmail.com>
2020-07-16 18:11:41 -0400

    My Create basic django project

---
+++ personal/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+
+# returns the request
+def home_screen_view(request):
+    print(request.headers)
+    return render(request, "base.html", {})
